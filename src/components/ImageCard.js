d7d14e4ebaa48cb509f7381b6dd950f38b1a45f0
Djordje Olujic <olujic.djordje@gmail.com>
2023-08-06 11:54:19 +0200

    commit

---
+++ src/components/ImageCard.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const ImageCard = ({ image }) => {
+  const tags = image.tags.split(',');
+  return (
+    <div className="px-3 py-3  my-2  bg-white max-w-sm rounded overflow-fidden shadow-lg">
+    <img src={image.webformatURL} alt='prop' className='w-full'/>
+    <div className='px-6 py-4'>
+      <div className='font-bold text-blue-500 text-xl mb-2'>
+        Photo by {image.user}
+      </div>
+      <ul>
+        <li>
+          <strong>Views: </strong>
+          {image.views}
+        </li>
+        <li>
+          <strong>Downloads: </strong>
+          {image.downloads}
+        </li>
+        <li>
+          <strong>Likes: </strong>
+          {image.likes}
+        </li>
+      </ul>
+    </div>
+      <div className="px-6 py-4">
+        {tags.map((tag, index) =>(
+                  <span key={index}  className=" text-white inline-block bg-blue-600 rounded-full px-3 py-1 m text-sm font-semibold  mx-2 my-2">
+                  #{tag}
+                </span>
+        ))}
+      </div>
+  </div>
+  )
+}
+
+export default ImageCard;
---
+++ src/components/ImageSearch.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const ImageSearch = ({ searchText }) => {
+    const [text, setText] = useState('');
+
+    const onSubmit =(e) => {
+        e.preventDefault();
+        searchText(text);
+    }
+
+  return (
+    <div className='flex max-w-sm w-full overflow-hidden mx-auto my-10'>
+
+        <div className='flex items-center px-2 py-3'>
+            <form  onSubmit={onSubmit} class="w-full max-w-sm">
+  <div className="flex items-center border-b border-[#C0-500 py-2">
+    <input onChange={e =>setText(e.target.value)} className="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Search for image..." />
+    <button className="flex-shrink-0 bg-blue-800 hover:bg-gray-700  text-sm  text-white py-2 px-2" type="submit">
+      Search
+    </button>
+  </div>
+</form>
+        </div>
+    </div>
+  )
+}
+
+export default ImageSearch;
